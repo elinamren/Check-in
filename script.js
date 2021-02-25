7a1a2a4b4e0848b60cf7c7e9428a2bd1c05539d2
Elin Amren <elin.amren@gmail.com>
2021-02-25 20:49:19 +0100

    styling and info window

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const questionHolder = document.getElementById("question-holder");
 const questionInput = document.getElementById("question-input");
 const addedByInput = document.getElementById("by-input");
 const formMessage = document.getElementById("form-message");
+const addContainer = document.getElementById("add-container");
+const messageContainer = document.getElementById("message-container");
+const infoContainer = document.getElementById("info-container");
+
+function openAddContainer() {
+  addContainer.classList.toggle("hidden");
+}
+
+function showInfo() {
+  infoContainer.classList.toggle("hidden");
+}
 
 //Get random checkin question
 function getRandomQuestion() {
@@ -36,8 +47,10 @@ function addQuestion() {
   })
     .then((r) => r.json())
     .then((data) => {
+      messageContainer.classList.remove("hidden");
+
       formMessage.innerText =
-        "Your awesome Check in question was added successfully, thank you!";
+        "Your awesome check in question was added successfully, thank you!";
       console.log(data);
     })
     .catch((error) => {
@@ -46,6 +59,26 @@ function addQuestion() {
       );
       console.log(error);
     });
-  questionInput.value = "";
-  addedByInput.value = "";
+  setTimeout(function () {
+    addContainer.classList.add("hidden");
+    messageContainer.classList.add("hidden");
+  }, 3000);
 }
+
+// Style button
+
+document.querySelectorAll("button").forEach((btn) =>
+  btn.addEventListener("mousedown", function (e) {
+    gsap.to(btn, { duration: 0.1, scale: 0.9 });
+  })
+);
+
+document.querySelectorAll("button").forEach((btn) =>
+  btn.addEventListener("mouseup", function (e) {
+    gsap.to(btn, {
+      duration: 0.5,
+      scale: 1,
+      ease: Elastic.easeOut.config(1, 0.2),
+    });
+  })
+);
